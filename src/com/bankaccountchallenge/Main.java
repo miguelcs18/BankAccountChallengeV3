a33435f9131607550b6e2f079436d443b1cd48cd
miguelcs18 <miguelcs18@gmail.com>
2022-08-07 15:31:59 -0500

    Add test data to test deposits and withdraws

--- src/com/bankaccountchallenge/Main.java
+++ src/com/bankaccountchallenge/Main.java
@@ -3,5 +3,13 @@ package com.bankaccountchallenge;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        BankAccount miguelAccount = new BankAccount();
+        miguelAccount.withdraw(100.0);
+
+        miguelAccount.deposit(50.0);
+        miguelAccount.withdraw(100.0);
+
+        miguelAccount.deposit(51.0);
+        miguelAccount.withdraw(100);
     }
 }
