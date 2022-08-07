e7e725edee5a8d909babf343f5d964a36f901bbf
miguelcs18 <miguelcs18@gmail.com>
2022-08-07 15:09:36 -0500

    Add class getters and setters

--- src/com/bankaccountchallenge/BankAccount.java
+++ src/com/bankaccountchallenge/BankAccount.java
@@ -7,4 +7,44 @@ public class BankAccount {
  private String email;
  private String phoneNumber;
 
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
