a92f434a9234d08762afe6f10383078bc6915402
miguelcs18 <miguelcs18@gmail.com>
2022-08-07 15:08:23 -0500

    Add class fields

--- src/com/bankaccountchallenge/BankAccount.java
+++ src/com/bankaccountchallenge/BankAccount.java
@@ -1,4 +1,10 @@
 package com.bankaccountchallenge;
 
 public class BankAccount {
+ private String number;
+ private double balance;
+ private String customerName;
+ private String email;
+ private String phoneNumber;
+
 }
