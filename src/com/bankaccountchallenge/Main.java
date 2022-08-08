686d7283473dc0fa2c0a8f753a5c0c46a385bb00
miguelcs18 <miguelcs18@gmail.com>
2022-08-07 22:50:53 -0500

    Set parameters to all args constructor

--- src/com/bankaccountchallenge/Main.java
+++ src/com/bankaccountchallenge/Main.java
@@ -3,7 +3,9 @@ package com.bankaccountchallenge;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        BankAccount miguelAccount = new BankAccount();
+        BankAccount miguelAccount = new BankAccount("12345",200.0,"Mike","mymail@gmail.com",
+                "333-333-333");
+        /*
         miguelAccount.withdraw(100.0);
 
         miguelAccount.deposit(50.0);
@@ -11,5 +13,6 @@ public class Main {
 
         miguelAccount.deposit(51.0);
         miguelAccount.withdraw(100);
+        */
     }
 }
