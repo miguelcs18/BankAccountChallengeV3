ddc519cf1747406858ca96f7b82bd46839ef2709
miguelcs18 <miguelcs18@gmail.com>
2022-08-07 22:56:57 -0500

    Uncomment test data

--- src/com/bankaccountchallenge/Main.java
+++ src/com/bankaccountchallenge/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         BankAccount miguelAccount = new BankAccount("12345",200.0,"Mike","mymail@gmail.com",
                 "333-333-333");
-        /*
+
         miguelAccount.withdraw(100.0);
 
         miguelAccount.deposit(50.0);
@@ -13,6 +13,5 @@ public class Main {
 
         miguelAccount.deposit(51.0);
         miguelAccount.withdraw(100);
-        */
     }
 }
