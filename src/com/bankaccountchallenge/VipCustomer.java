744bc3b6f11f7f5ea254050d9738fe64c17a0b20
miguelcs18 <miguelcs18@gmail.com>
2022-08-07 23:40:48 -0500

    Getters added

--- src/com/bankaccountchallenge/VipCustomer.java
+++ src/com/bankaccountchallenge/VipCustomer.java
@@ -6,7 +6,28 @@ public class VipCustomer {
     private String email;
 
     public VipCustomer() {
-
+        this("default name",10000.0,"default@email.com");
+    }
+    public VipCustomer(String name, double creditLimit) {
+        this(name,creditLimit,"default@email.com");
+        this.name = name;
+        this.creditLimit = creditLimit;
+    }
+    public VipCustomer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
